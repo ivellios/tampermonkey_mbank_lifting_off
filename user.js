e9f765efec4fe4711c427acac02eabcab933cb19
Janusz Kamieński <200957+ivellios@users.noreply.github.com>
2018-05-23 14:17:51 +0200

    Added tamper monkey script

---
+++ user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         mBank no-lifting
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Fixes mBank interface background and top bar by removing class "lifting" from body element and adding background from demo.
+// @author       Janusz "Ivellios" Kamieński
+// @match        *://online.mbank.pl/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/ivellios/tampermonkey_mbank_lifting_off/master/user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.remove("lifting");
+    body.style.backgroundImage = "url(http://nowedemo.mbank.pl/gfx/bg/bg_content.png)";
+})();
